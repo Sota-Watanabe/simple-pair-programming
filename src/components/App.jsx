41a90334050e8e0470752c77689e66495a1b4eb2
Sota-Watanabe <ma20112@shibaura-it.ac.jp>
2020-12-11 23:58:48 +0900

    h1追加

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import Connection from "./Connection";
 const App = () => {
   return (
     <>
+      <h1 style={{textAlign: "center", padding: "50px"}}>Simple Pair Programming</h1>
       <Connection />
     </>
   );
