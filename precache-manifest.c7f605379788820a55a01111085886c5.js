679551a079aefc45a30f6ef039cd30f45e5d7536
Sota-Watanabe <Sota-Watanabe@users.noreply.github.com>
2020-12-12 08:42:24 +0000

    deploy: 47e737907416b14d4d432369651d152aa415e4f5

---
+++ precache-manifest.c7f605379788820a55a01111085886c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4422b6fb86956e9158cc53caf3585650",
+    "url": "/simple-pair-programming/index.html"
+  },
+  {
+    "revision": "6d6dca5720438a850870",
+    "url": "/simple-pair-programming/static/js/2.fac96e78.chunk.js"
+  },
+  {
+    "revision": "048348a0047202ed76cf784affee46f2",
+    "url": "/simple-pair-programming/static/js/2.fac96e78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3772868c11c573833bbb",
+    "url": "/simple-pair-programming/static/js/main.5784014c.chunk.js"
+  },
+  {
+    "revision": "2e55ddecbe72ea4d4571",
+    "url": "/simple-pair-programming/static/js/runtime-main.81df54d1.js"
+  }
+]);
